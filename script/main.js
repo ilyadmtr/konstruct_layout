cee2ad7a37c59a0ee9dc0ab54276dbb88a2e4770
ilyadmtr <ilyadmytrenko@gmail.com>
2021-11-29 17:41:15 +0200

    added header and one section

---
+++ script/main.js
@@ -0,0 +1,9 @@
+easy_background(
+  ".head_bg",
+
+  {
+    slide: ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg"],
+
+    delay: [3500, 3500, 3500, 3500, 3500],
+  }
+);
